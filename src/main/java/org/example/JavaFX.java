b18cb5c2819ad8d9e5d2845ff63e3ac62a3761cd
Jay Gerners <20127235@student.hhs.nl>
2023-04-04 14:43:44 +0200

    Java FX instal

---
+++ src/main/java/org/example/JavaFX.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main (String[] args){
+
+    }
+}
